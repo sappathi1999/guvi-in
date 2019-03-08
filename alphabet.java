55f03a27740ed3ddb387d1ca3dd428902492fed6
archana <47376756+sappathi1999@users.noreply.github.com>
2019-03-08 02:44:31 -0800

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class alphabets
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	char ch;
+	for(ch='a';ch<='z';ch++)
+	System.out.println(ch);
+	}
+}
